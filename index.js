6065c003e68c86a60b2f93417b8e2d1188a7fa18
Chris Thomason <christhomason@Chriss-MacBook-Pro.local>
2021-05-27 12:33:19 +0100

    Final update of the Chrome Lead Extension

--- index.js
+++ index.js
@@ -1,28 +1,60 @@
-myLeads = [];
-const inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById("input-btn");
-const ulEl = document.getElementById("ul-el");
+let myLeads = []
+let oldLeads = []
+let tabs = [
+    {
+        url: "https://www.linkedin.com/in/per-harald-borgen/",
+    }
+]
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
+const ulEl = document.getElementById("ul-el")
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
+if (leadsFromLocalStorage !== null){
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
 
-inputBtn.addEventListener("click",function() {
-    myLeads.push(inputEl.value);
-    inputEl.value = "";
-    renderLeads();
-})
-
-
-function renderLeads(){
+function render(leads){
     let listItems = ""
-    for (let i = 0; i<myLeads.length; i++) {
+    for (let i = 0; i<leads.length; i++) {
         listItems += `
         <li>
-            <a href=${myLeads[i]} target="_blank"> 
-                ${myLeads[i]} 
+            <a href=${leads[i]} target="_blank"> 
+                ${leads[i]} 
             </a>
         </li>
         
         `;
     }
 
-    ulEl.innerHTML = listItems;
+    ulEl.innerHTML = listItems
 }
+
+inputBtn.addEventListener("click",function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+tabBtn.addEventListener("dblclick", function() {
+  
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+  
+
+  
+
+})
